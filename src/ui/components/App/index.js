09db0b246461bc39ec76cce54fd683ac5c8a407b
Laura Tannahill <lntannahill@gmail.com>
2021-09-13 18:50:57 -0300

    Add scaffolding for watch and iPhone client routes

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '../Home';
+import Watches from '../Watches';
+import IPhones from '../IPhones';
 import NotFound from '../NotFound';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/">
+      <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="/watches">
+        <Watches />
+      </Route>
+      <Route path="/iphones">
+        <IPhones />
       </Route>
     </Switch>
   </Router>
