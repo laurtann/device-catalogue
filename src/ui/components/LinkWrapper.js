5f900c56f393676e303d99eb59d41cd51c34dcd3
Laura Tannahill <lntannahill@gmail.com>
2021-09-14 12:16:24 -0300

    Add wrapper for link components to pass in react router

---
+++ src/ui/components/LinkWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+// Adapted from https://tds.telus.com/components/index.html#/Links?id=link
+const LinkWrapper = ({ TDSLink, children, ...rest }) => (
+  <TDSLink {...rest} reactRouterLinkComponent={rest.to ? ReactRouterLink : undefined}>
+    {children}
+  </TDSLink>
+)
+
+export default LinkWrapper
