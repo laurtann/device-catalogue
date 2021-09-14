014274f74339250755700e7882724d3ba2c0ef25
Laura Tannahill <lntannahill@gmail.com>
2021-09-13 21:43:47 -0300

    Add basic links to index page to reach MVP on front end

--- src/ui/components/Home/index.js
+++ src/ui/components/Home/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Heading from '@tds/core-heading';
+import { Link } from 'react-router-dom';
 
-const Home = () => <Heading level="h1">Device Catalogue!</Heading>;
+const Home = () => {
+  return (
+    <Fragment>
+      <Heading level="h1">Device Catalogue!</Heading>
+      <Link to="/iphones">iPhones</Link>
+      <Link to="/iphones">Watches</Link>
+    </Fragment>
+  )
+};
 
 export default Home;
